4081bcb2a8c2b23fc571c1af862336bbac9d7168
hogehogeBrav <zmzmakoto@icloud.com>
2022-12-26 00:34:43 +0900

    [update] ファイルアップロード画面

---
+++ node/index.js
@@ -0,0 +1,35 @@
+"use strict"
+const db = require("./settings/db.js");
+const express = require('express');
+const app = express();
+const http_socket = require('http').Server(app);
+const io_socket = require('socket.io')(http_socket);
+const passport = require('passport');
+const mysql = require('mysql2');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + "/views" , {index: false}));
+app.use(express.static(__dirname + "/public" , {index: false}));
+app.use(express.static(__dirname + "/js" , {index: false}));
+app.use(express.static(__dirname + "/css" , {index: false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+io_socket.on('connection', function(socket){
+  console.log('connected');
+  socket.on('c2s' , function(msg){
+    io_socket.to(msg.auctionid).emit('s2c', msg);
+  });
+  socket.on('c2s-join', function(msg){
+    console.log('c2s-join:' + msg.auctionid);
+    socket.join(msg.auctionid);
+  });
+});
+
+
+http_socket.listen(19000);
