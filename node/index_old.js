692e1e45e9ead159366e8f87ed75c344d0de1f16
hogehogeBrav <zmzmakoto@icloud.com>
2023-01-18 22:14:52 +0900

    [add] aws s3 ファイルアップロード

--- node/index_old.js
+++ node/index_old.js
@@ -1,5 +1,4 @@
 "use strict"
-require('dotenv').config();
 const db = require("./settings/db.js");
 const express = require('express');
 const app = express();
@@ -7,17 +6,8 @@ const http_socket = require('http').Server(app);
 const io_socket = require('socket.io')(http_socket);
 const passport = require('passport');
 const mysql = require('mysql2');
-const aws = require('aws-sdk');
-const fs = require('fs');
 
 app.set('view engine', 'ejs');
-
-const s3 = new aws.S3({
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	region: process.env.AWS_REGION,
-	signatureVersion: 'v4'
-});
 
 app.use(express.static(__dirname + "/views" , {index: false}));
 app.use(express.static(__dirname + "/public" , {index: false}));
@@ -37,39 +27,21 @@ app.get('/upload', (req, res) => {
 
 // ファイルアップロード
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-
-// multer-s3の公式を参照（下記）
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    ACL: 'public-read',
-    bucket: 'rakushare',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
-    },
-    key: function (req, file, cb) {
-      cb(null, "testuser" + "/" + Buffer.from(file.originalname, 'latin1').toString('utf8',))
-    },
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-  })
+// const strage = multer.diskStorage({
+//   destination: function(req, file, cb){
+//     cb(null, __dirname + '/uploads/');
+//   },
+//   filename: function(req, file, cb){
+//     cb(null, Buffer.from(file.originalname, 'latin1').toString('utf8',));
+//   }
+// });
+// const upload = multer({storage: strage});
+const upload = multer({dest: __dirname + '/uploads/'});
+app.post('/upload', upload.single('file'), (req, res) => {
+  console.log(req.file);
+  console.log(Buffer.from(req.file.originalname, 'latin1').toString('utf8',));
+  res.send('ok');
 });
-
-// Create images
-// upload(...)で画像を登録。multer公式参照（下記）
-app.post('/upload', upload.single('file'), (req, res, next) => {
-  if (req.file) {
-    console.log('Image uploaded!');
-    // res.redirect('/images');
-    res.send('ok');
-  }
-  else {
-    console.log('Image can not uploaded!')
-    // res.render('image.ejs');
-    res.send('ok');
-  }
-})
-
 
 io_socket.on('connection', function(socket){
   console.log('connected');
