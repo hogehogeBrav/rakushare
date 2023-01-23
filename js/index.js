890dea4417ee49ccb4eab5b4b38b75765a1a62c9
hogehogeBrav <zmzmakoto@icloud.com>
2023-01-23 13:52:38 +0900

    [add] 使い方モーダルと設定モーダル追加

--- js/index.js
+++ js/index.js
@@ -17,3 +17,29 @@ $(document).on('click', '#upload_cancel', function(event) {
   event.preventDefault();
   $('#upload_modal').iziModal('close');
 });
+
+// ダウンロードモーダル
+
+// 設定モーダル
+$(document).on('click', '#settings', function(event) {
+  event.preventDefault();
+  $('#setting_modal').iziModal('open');
+});
+$('#setting_modal').iziModal({
+  title: '設定',
+  subtitle: '設定を変更できます',
+  headerColor: "#0f9574",
+  radius: 10,
+});
+
+// 使い方モーダル
+$(document).on('click', '#info', function(event) {
+  event.preventDefault();
+  $('#info_modal').iziModal('open');
+});
+$('#info_modal').iziModal({
+  title: '使い方',
+  subtitle: '使い方を説明します',
+  headerColor: "#0f9574",
+  radius: 10,
+});
