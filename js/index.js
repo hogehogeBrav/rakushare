3ffa95dc1bbe234acff49ffe7dfaf485e3b0b3c1
hogehogeBrav <zmzmakoto@icloud.com>
2023-01-23 15:43:34 +0900

    [add] パスワードチェック、toastr追加

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 // button
 const button = document.querySelector('button');
 
+//ドキュメントロード時に、toastr のオプションを設定する
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-bottom-center",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "1000000000",
+  "extendedTimeOut": "1000000000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "tapToDismiss": false
+}
+
 // アップロードモーダル
 $(document).on('click', '#upload', function(event) {
   event.preventDefault();
@@ -43,3 +61,8 @@ $('#info_modal').iziModal({
   headerColor: "#0f9574",
   radius: 10,
 });
+
+// アップロードエラー表示部
+if(upload_error == 1) toastr.error('以前に同じ名前で作成した場合は、「ルームを見る」でルームを確認してください', 'このルーム名は既に使用されています！');
+if(upload_error == 2) toastr.error('ルーム名とアクセスパスワードを入力してください', '入力エラー');
+if(upload_error == 3) toastr.error('アクセスパスワードは半角英数字で入力してください', '入力エラー');
